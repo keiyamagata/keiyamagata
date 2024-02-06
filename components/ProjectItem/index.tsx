f42d8354d9fdaef49de708a496b7708f573e8c49
keiyamagata <kei.yamagata@icloud.com>
2024-02-06 13:58:49 +0100

    mobile view update

--- components/ProjectItem/index.tsx
+++ components/ProjectItem/index.tsx
@@ -13,8 +13,12 @@ const Project: React.FC<ProjectProps> = ({ name, techStack, link }) => {
   return (
     <article className="border-t border-primary group">
       <a href={link} target="_blank" rel="noopener noreferrer">
-        <div className="h-40 grid grid-cols-3 place-items-center">
-          <Typography type="project-title" text={name} />
+        <div className="h-60 h- md:h-40 grid grid-cols-1 md:grid-cols-3 md:place-items-center">
+          <Typography
+            type="project-title"
+            text={name}
+            className="pt-4 md:pt-0"
+          />
           <div>
             {techStack.map((item, i) => (
               <Typography
@@ -25,9 +29,9 @@ const Project: React.FC<ProjectProps> = ({ name, techStack, link }) => {
               />
             ))}
           </div>
-          <div className="flex items-center">
-            <p className="text-3xl">Visit</p>
-            <Arrow className="w-12 h-12 ml-4 group-hover:translate-x-4 duration-150" />
+          <div className="flex items-center place-self-end md:place-self-auto pb-4 md:pb-0">
+            <p className="text-xl md:text-2xl uppercase">Visit</p>
+            <Arrow className="w-7 h-7 md:w-9 md:h-9 ml-4 group-hover:translate-x-3 duration-150" />
           </div>
         </div>
       </a>
