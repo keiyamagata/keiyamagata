ff9d060937f719b6df3d5cadc1045e7d42cdc493
keiyamagata <kei.yamagata@icloud.com>
2024-02-06 13:50:14 +0100

    minor change

--- components/Navigation/Navbar.tsx
+++ components/Navigation/Navbar.tsx
@@ -12,7 +12,7 @@ const NavBar = () => {
 
   return (
     <header>
-      <nav className="w-[22rem] md:w-[29.25rem] fixed top-4 left-1/2 -translate-x-1/2 rounded-full border border-primary bg-secondary/50 backdrop-blur-sm z-[99]">
+      <nav className="w-[21.6rem] md:w-[29.3rem] fixed top-4 left-1/2 -translate-x-1/2 rounded-full border border-primary bg-secondary/50 backdrop-blur-sm z-[99]">
         <ul className="w-full relative py-2.5 flex justify-center items-center">
           {navItems.map((item) => {
             const isActive = item.path === currentView;
@@ -21,7 +21,7 @@ const NavBar = () => {
               <li key={item.path}>
                 <Link
                   href={`#${item.path}`}
-                  className="relative w-full px-3 py-2 rounded-full uppercase text-sm md:text-base text-primary focus-visible:outline focus-visible:outline-primary/80"
+                  className="relative w-full px-3 md:px-6 py-2 rounded-full uppercase text-sm md:text-base text-primary focus-visible:outline focus-visible:outline-primary/80"
                   data-active={isActive}
                   onMouseOver={() => setHoveredPath(item.path)}
                   onMouseLeave={() => setHoveredPath(currentView)}
